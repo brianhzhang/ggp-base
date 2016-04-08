38839931602e0179e4f6338bb79a057155de41c5
brianhzhang <zhangbrianh@gmail.com>
2016-04-08 10:16:52 -0700

    MyPlayer plays legally (or randomly, if you so desire)

--- player/MyPlayer.java
+++ player/MyPlayer.java
@@ -1,47 +1,55 @@
+import java.util.List;
+import java.util.Random;
+
 import org.ggp.base.player.gamer.exception.GamePreviewException;
 import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
 import org.ggp.base.util.game.Game;
 import org.ggp.base.util.statemachine.Move;
 import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.cache.CachedStateMachine;
 import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
 
 public class MyPlayer extends StateMachineGamer {
 
-	@Override
+	private final Random random = new Random();
+
 	public StateMachine getInitialStateMachine() {
-		return null;
+		return new CachedStateMachine(new ProverStateMachine());
 	}
 
-	@Override
 	public void stateMachineMetaGame(long timeout)
-			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
-
+			throws TransitionDefinitionException, MoveDefinitionException,
+			GoalDefinitionException {
+		return;
 	}
 
-	@Override
 	public Move stateMachineSelectMove(long timeout)
-			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
-		return null;
+			throws TransitionDefinitionException, MoveDefinitionException,
+			GoalDefinitionException {
+		List<Move> moves = getStateMachine().findLegals(getRole(),
+				getCurrentState());
+		// legal player #1: 8432
+		// legal player #2: 1344
+		return moves.get(0); // legal player
+		// return moves.get(random.nextInt(moves.size())); // random player
+
 	}
 
-	@Override
 	public void stateMachineStop() {
-
+		return;
 	}
 
-	@Override
 	public void stateMachineAbort() {
-
+		return;
 	}
 
-	@Override
 	public void preview(Game g, long timeout) throws GamePreviewException {
-
+		return;
 	}
 
-	@Override
 	public String getName() {
 		return "Brian and Jeff'); DROP TABLE TEAMS; --";
 	}
