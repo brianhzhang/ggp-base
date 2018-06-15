dd42919d81ce6ee04ca2d48992c51a522f4c8c15
brianhzhang <zhangbrianh@gmail.com>
2018-06-15 14:57:44 -0700

    Sort moves in HumanDetailPanel

--- src/main/java/org/ggp/base/player/gamer/statemachine/human/gui/HumanDetailPanel.java
+++ src/main/java/org/ggp/base/player/gamer/statemachine/human/gui/HumanDetailPanel.java
@@ -4,6 +4,9 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import javax.swing.AbstractAction;
 import javax.swing.JButton;
@@ -21,105 +24,95 @@ import org.ggp.base.util.statemachine.Move;
 import org.ggp.base.util.ui.table.JZebraTable;
 import org.ggp.base.util.ui.timer.JTimerBar;
 
-
 @SuppressWarnings("serial")
-public final class HumanDetailPanel extends DetailPanel
-{
+public final class HumanDetailPanel extends DetailPanel {
 
-    private final JZebraTable moveTable;
-    private final JTextField moveTextField;
-    private final JButton selectButton;
-    private Move selection;
-    private final JTimerBar timerBar;
+	private final JZebraTable moveTable;
+	private final JTextField moveTextField;
+	private final JButton selectButton;
+	private Move selection;
+	private final JTimerBar timerBar;
 
-    public HumanDetailPanel()
-    {
-        super(new GridBagLayout());
+	public HumanDetailPanel() {
+		super(new GridBagLayout());
 
-        DefaultTableModel model = new DefaultTableModel();
-        model.addColumn("Legal Moves");
+		DefaultTableModel model = new DefaultTableModel();
+		model.addColumn("Legal Moves");
 
-        moveTable = new JZebraTable(model)
-        {
+		moveTable = new JZebraTable(model) {
 
-            @Override
-            public boolean isCellEditable(int rowIndex, int colIndex)
-            {
-                return false;
-            }
-        };
-        selectButton = new JButton(selectButtonMethod());
-        moveTextField = new JTextField();
-        timerBar = new JTimerBar();
-        selection = null;
+			@Override
+			public boolean isCellEditable(int rowIndex, int colIndex) {
+				return false;
+			}
+		};
+		selectButton = new JButton(selectButtonMethod());
+		moveTextField = new JTextField();
+		timerBar = new JTimerBar();
+		selection = null;
 
-        moveTable.setShowHorizontalLines(true);
-        moveTable.setShowVerticalLines(true);
-        moveTextField.setEditable(false);
+		moveTable.setShowHorizontalLines(true);
+		moveTable.setShowVerticalLines(true);
+		moveTextField.setEditable(false);
 
-        this.add(new JScrollPane(moveTable, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED), new GridBagConstraints(0, 0, 2, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 5, 5));
-        this.add(selectButton, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0));
-        this.add(moveTextField, new GridBagConstraints(1, 1, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 5, 5));
-        this.add(timerBar, new GridBagConstraints(0, 2, 2, 1, 1.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 5, 5));
-    }
+		this.add(
+				new JScrollPane(moveTable, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+						ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED),
+				new GridBagConstraints(0, 0, 2, 1, 1.0, 1.0, GridBagConstraints.CENTER,
+						GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 5, 5));
+		this.add(selectButton, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER,
+				GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0));
+		this.add(moveTextField, new GridBagConstraints(1, 1, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER,
+				GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 5, 5));
+		this.add(timerBar, new GridBagConstraints(0, 2, 2, 1, 1.0, 0.0, GridBagConstraints.CENTER,
+				GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 5, 5));
+	}
 
-    @Override
-    public void observe(Event event)
-    {
-        if (event instanceof HumanNewMovesEvent)
-        {
-            observe((HumanNewMovesEvent) event);
-        }
-        else if (event instanceof HumanTimeoutEvent)
-        {
-            observe((HumanTimeoutEvent) event);
-        }
-        else if (event instanceof PlayerTimeEvent)
-        {
-            observe((PlayerTimeEvent) event);
-        }
-    }
+	@Override
+	public void observe(Event event) {
+		if (event instanceof HumanNewMovesEvent) {
+			observe((HumanNewMovesEvent) event);
+		} else if (event instanceof HumanTimeoutEvent) {
+			observe((HumanTimeoutEvent) event);
+		} else if (event instanceof PlayerTimeEvent) {
+			observe((PlayerTimeEvent) event);
+		}
+	}
 
-    private void observe(HumanNewMovesEvent event)
-    {
-        DefaultTableModel model = (DefaultTableModel) moveTable.getModel();
-        model.setRowCount(0);
-        for (Move move : event.getMoves())
-        {
-            model.addRow(new Move[] { move });
-        }
+	private void observe(HumanNewMovesEvent event) {
+		DefaultTableModel model = (DefaultTableModel) moveTable.getModel();
+		model.setRowCount(0);
+		List<Move> moves = new ArrayList<>(event.getMoves());
+		Collections.sort(moves, (a, b) -> a.toString().compareTo(b.toString()));
+		for (Move move : moves) {
+			model.addRow(new Move[] { move });
+		}
 
-        selection = event.getSelection();
-        moveTextField.setText(selection.toString());
-    }
+		selection = event.getSelection();
+		moveTextField.setText(selection.toString());
+	}
 
-    private void observe(HumanTimeoutEvent event)
-    {
-        event.getHumanPlayer().setMove(selection);
-    }
+	private void observe(HumanTimeoutEvent event) {
+		event.getHumanPlayer().setMove(selection);
+	}
 
-    private void observe(PlayerTimeEvent event)
-    {
-        timerBar.time(event.getTime(), 500);
-    }
+	private void observe(PlayerTimeEvent event) {
+		timerBar.time(event.getTime(), 500);
+	}
 
-    private AbstractAction selectButtonMethod()
-    {
-        return new AbstractAction("Select")
-        {
+	private AbstractAction selectButtonMethod() {
+		return new AbstractAction("Select") {
 
-            @Override
-            public void actionPerformed(ActionEvent evt)
-            {
-                int row = moveTable.getSelectedRow();
-                if (row != -1)
-                {
-                    DefaultTableModel model = (DefaultTableModel) moveTable.getModel();
-                    selection = (Move) model.getValueAt(row, 0);
-                    moveTextField.setText(selection.toString());
-                }
-            }
-        };
-    }
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				int row = moveTable.getSelectedRow();
+				if (row != -1) {
+					DefaultTableModel model = (DefaultTableModel) moveTable.getModel();
+					selection = (Move) model.getValueAt(row, 0);
+					moveTextField.setText(selection.toString());
+				}
+			}
+		};
+	}
 
 }
